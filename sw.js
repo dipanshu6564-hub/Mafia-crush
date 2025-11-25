0457d7ab00315be8f30b50f5ce065b4762acc411
dipanshu6564-hub <dipanshu6564@gmail.com>
2025-11-25 09:08:35 +0530

    feat: Implement PWA and integrate ads
    Integrates PWA manifest and service worker for offline caching and standalone app experience. Also adds ad scripts for monetization.

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'mafia-crush-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './index.tsx',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        // We try to cache core files, but don't fail if specific files aren't found in dev env
+        return cache.addAll(urlsToCache).catch(err => console.log('Cache addAll error', err));
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
